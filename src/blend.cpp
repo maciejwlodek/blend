2382c921e794f980a141b674ee3211ee8fa8dc2a
Maciej Wlodek <maciej.r.wlodek@gmail.com>
2019-07-08 11:47:42 -0400

    added kmeans mode for s6dist

--- src/blend.cpp
+++ src/blend.cpp
@@ -360,6 +360,7 @@ int main(int argc, char* argv[])
   std::string R_program0S6 = home+"/R/blend0S6.R";
   std::string R_programK = home+"/R/blendK.R";
   std::string R_programKNC = home+"/R/blendKNC.R";
+  std::string R_programKS6 = home+"/R/blendKS6.R";
   std::string R_program1 = home+"/R/blend1.R";
   std::string R_program1NC = home+"/R/blend1NC.R";
   std::string R_program1SF = home+"/R/blend1SF.R";
@@ -395,6 +396,7 @@ int main(int argc, char* argv[])
       mode_string != "-aSF" && mode_string != "-aDOSF" &&                          // possibly with SF appended
       mode_string != "-aS6" && mode_string != "-aDOS6" &&                          // possibly with S6 appended
       mode_string != "-k" && mode_string != "-kNC" &&                              // or kmeans mode
+      mode_string != "-kS6" &&
       mode_string != "-s" && mode_string != "-sLCV" && mode_string != "-saLCV" &&  // or "-s" or "-sLCV" or "-saLCV"
       mode_string != "-sNC" && mode_string != "-sLCVNC" && mode_string != "-saLCVNC" &&  // or "-sNC" or "-sLCVNC" or "-saLCVNC"
       mode_string != "-sSF" && mode_string != "-sLCVSF" && mode_string != "-saLCVSF" &&  // or "-sSF" or "-sLCVSF" or "-saLCVSF"
@@ -420,7 +422,8 @@ int main(int argc, char* argv[])
       || mode_string == "-aS6" 
       || mode_string == "-aDOS6"
       || mode_string == "-k"
-      || mode_string == "-kNC")     // Analysis pass
+      || mode_string == "-kNC"
+      || mode_string == "-kS6")     // Analysis pass
   {
    // In order to line up BLEND with the way ccp4i works (with stdin passed keywords) this is what has been added
 
@@ -1085,6 +1088,10 @@ int main(int argc, char* argv[])
    {
      std::cout << "You are now running BLEND in k-means mode using NCDist." << std::endl;
    }
+   if (mode_string == "-kS6")
+   {
+     std::cout << "You are no running BLEND in k-means mode using S6Dist." << std::endl;
+   }
 
    std::cout << std::endl;
    std::cout << "<!--SUMMARY_END--></FONT></B>" << std::endl;
@@ -1297,8 +1304,10 @@ int main(int argc, char* argv[])
                                                        spacegroup_class,crystal_flag,R_program0S6,Rscp,mode_string);
     if (mode_string == "-k") statistics_with_R2(hkl_list, sg_to_crystal, crystal_to_centering,
                                                         spacegroup_class, crystal_flag, R_programK, Rscp, mode_string);
-    if (mode_string == "-kNC") statistics_with_R2(hkl_list, sg_to_crystal, crystal_to_centering, 
+    if (mode_string == "-kNC") statistics_with_R2(hkl_list, sg_to_crystal, crystal_to_centering,
                                                         spacegroup_class, crystal_flag, R_programKNC, Rscp, mode_string);
+    if (mode_string == "-kS6") statistics_with_R2(hkl_list, sg_to_crystal, crystal_to_centering,
+                                                        spacegroup_class, crystal_flag, R_programKS6, Rscp, mode_string);
    }
 
    // Delete BLEND_KEYWORDS.dat before termination
